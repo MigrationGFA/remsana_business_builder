88df498f28ccd3698ec7fb01de70e71f696df562
MigrationGFA <159458244+MigrationGFA@users.noreply.github.com>
2026-03-07 20:30:02 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,11 +15,5 @@ export default defineConfig({
   },
   server: {
     port: 5174,
-    proxy: {
-      '/api': {
-        target: 'https://rbbphpremsana-test.azurewebsites.net',
-        changeOrigin: true,
-      }
-    }
   }
 })
